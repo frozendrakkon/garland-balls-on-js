369f472738be910446d7258fa18d8f480b376523
sergei alekseev <52599099+frozendrakkon@users.noreply.github.com>
2020-12-24 19:51:41 +0300

    js

---
+++ code.js
@@ -0,0 +1,13 @@
+const yellow = document.getElementById("yellow")
+const blue = document.getElementById("blue")
+const green = document.getElementById("green")
+const red = document.getElementById("red")
+const tumblerOff = document.getElementById("tumbler")
+
+
+const doTumblerOff = ((tumblerOff) => {
+
+    const EventClickTumbler = tumblerOff.document.addEventListener("click" function() {
+        yellow = yellow
+    })
+})
