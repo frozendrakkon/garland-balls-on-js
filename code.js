d18e198b6b5d6954e30378e664ff6dfde8bb8336
sergei alekseev <52599099+frozendrakkon@users.noreply.github.com>
2020-12-29 21:35:13 +0300

    nothing

--- code.js
+++ code.js
@@ -2,12 +2,15 @@ const yellow = document.getElementById("yellow")
 const blue = document.getElementById("blue")
 const green = document.getElementById("green")
 const red = document.getElementById("red")
-const tumblerOff = document.getElementById("tumbler")
 
 
-const doTumblerOff = ((tumblerOff) => {
+const doTumblerOffOn = (() => {
+    document.getElementById("tumbler").onclick = function() {
+         yellow.classList.toggle("glow-yellow")
+         blue.classList.toggle("glow-blue")
+         green.classList.toggle("glow-green")
+         red.classList.toggle("glow-red")
+    }
 
-    const EventClickTumbler = tumblerOff.document.addEventListener("click" function() {
-        yellow = yellow
-    })
 })
+doTumblerOffOn()
