8d7b3a0391f3a7a875c4c5ca08ee6e47e958feaf
unknown <frozedrakon@gmail.com>
2020-12-31 16:25:04 +0300

    add interactive

--- code.js
+++ code.js
@@ -1,97 +1,105 @@
-const yellow = document.getElementsByClassName("yellow")
-const blue = document.getElementsByClassName("blue")
-const green = document.getElementsByClassName("green")
-const red = document.getElementsByClassName("red")
+const yellow = document.getElementsByClassName("yellow");
+const blue = document.getElementsByClassName("blue");
+const green = document.getElementsByClassName("green");
+const red = document.getElementsByClassName("red");
 
-const btnTumbler = document.getElementById("tumbler")
-const btnMode = document.getElementById("mode")
-const btnLight = document.getElementById("light")
-
+const btnTumbler = document.getElementById("tumbler");
+const btnMode = document.getElementById("mode");
+const btnLight = document.getElementById("light");
 
 function GarlandGlowYellow(color) {
-    for(let i = 0; i < color.length; i++) {
-        color[i].classList.add("glow-yellow")
-    }
+  for (let i = 0; i < color.length; i++) {
+    color[i].classList.add("glow-yellow");
+  }
 }
 
 function GarlandGlowBlue(color) {
-    for(let i = 0; i < color.length; i++) {
-        color[i].classList.add("glow-blue")
-    }
+  for (let i = 0; i < color.length; i++) {
+    color[i].classList.add("glow-blue");
+  }
 }
 
 function GarlandGlowGreen(color) {
-    for(let i = 0; i < color.length; i++) {
-        color[i].classList.add(`glow-green`)
-    }
+  for (let i = 0; i < color.length; i++) {
+    color[i].classList.add(`glow-green`);
+  }
 }
 
 function GarlandGlowRed(color) {
-    for(let i = 0; i < color.length; i++) {
-        color[i].classList.add("glow-red")
-    }
+  for (let i = 0; i < color.length; i++) {
+    color[i].classList.add("glow-red");
+  }
 }
 
 // убираем свечение
 function GarlandDecay(color, glow) {
-    for(let i = 0; i < color.length; i++) {
-        color[i].classList.remove(`glow-${glow}`)
-    }
-} 
+  for (let i = 0; i < color.length; i++) {
+    color[i].classList.remove(`glow-${glow}`);
+  }
+}
 
+let modeAlternateCountClick = 1;
 
 function modeAlternate() {
-    
-    btnMode.onclick = function () {
 
-        setTimeout(() => GarlandGlowYellow(yellow), 200);
-        setTimeout(() => GarlandGlowBlue(blue), 400);
-        setTimeout(() => GarlandGlowGreen(green), 600);
-        setTimeout(() => GarlandGlowRed(red), 800);
-        
-        setTimeout(() => {
-            GarlandDecay(yellow, "yellow")
-            GarlandDecay(blue, "blue")
-            GarlandDecay(green, "green")
-            GarlandDecay(red, "red")
-        }, 1000);
-    }
+  btnMode.onclick = function () {
+  modeAlternateCountClick += 1
+
+    setInterval(() => {
+      setTimeout(() => GarlandGlowYellow(yellow), 200);
+      setTimeout(() => GarlandGlowBlue(blue), 300);
+      setTimeout(() => GarlandGlowGreen(green), 400);
+      setTimeout(() => GarlandGlowRed(red), 500);
+
+      setTimeout(() => {
+        GarlandDecay(yellow, "yellow");
+        GarlandDecay(blue, "blue");
+        GarlandDecay(green, "green");
+        GarlandDecay(red, "red");
+      }, 700);
+    }, 800);
+  };
 }
+
+
 
 let TumblerCountClick = 0; // Считаем сколько раз была нажата кнопка => включение / выключение
 
-const TumblerOnOff = (() => {
+const TumblerOnOff = () => {
+  btnTumbler.onclick = function () {
+    TumblerCountClick += 1;
+    if (TumblerCountClick % 2 !== 0) {
+      GarlandGlowYellow(yellow);
+      GarlandGlowBlue(blue);
+      GarlandGlowGreen(green);
+      GarlandGlowRed(red);
+      btnTumbler.innerHTML = "OFF";
 
-    btnTumbler.onclick = function() {
-        TumblerCountClick += 1
-         if (TumblerCountClick % 2 !== 0 ) {
-            GarlandGlowYellow(yellow)
-            GarlandGlowBlue(blue)
-            GarlandGlowGreen(green)
-            GarlandGlowRed(red)
-         } else {
-            GarlandDecay(yellow, "yellow")
-            GarlandDecay(blue, "blue")
-            GarlandDecay(green, "green")
-            GarlandDecay(red, "red")
-            
-         }
+    } else {
+      GarlandDecay(yellow, "yellow");
+      GarlandDecay(blue, "blue");
+      GarlandDecay(green, "green");
+      GarlandDecay(red, "red");
+      btnTumbler.innerHTML = "ON";
     }
-})
+  };
+};
 
-let CountLightClick = 1
+let CountLightClick = 1;
 
-const lightOffOnn = (() => {
-    btnLight.onclick = function() {
-        CountLightClick += 1;
-        if(CountLightClick % 2 !== 0 ) {
-            document.body.style.backgroundColor = "#6699FF"
-        } else {
-            document.body.style.backgroundColor = "black"
-        }
+const lightOffOnn = () => {
+  btnLight.onclick = function () {
+    CountLightClick += 1;
+    if (CountLightClick % 2 !== 0) {
+      document.body.style.backgroundColor = "#6699FF";
+      btnLight.innerHTML = "Night";
+    } else {
+      document.body.style.backgroundColor = "black";
+      btnLight.innerHTML = "Light";
     }
-})
+  };
+};
 
-TumblerOnOff()
-modeAlternate()
-lightOffOnn()
+TumblerOnOff();
+modeAlternate();
+lightOffOnn();
